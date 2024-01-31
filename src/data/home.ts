eed2491e3578683570dd7d8daba3fd389370a512
Olivia <olivia@chameleoncreator.com>
2024-01-31 15:54:00 +1300

    init commit

---
+++ src/data/home.ts
@@ -0,0 +1,14 @@
+export const categories = [
+  'All',
+  'JavaScript',
+  'TypeScript ',
+  'Programming',
+  'Lana Del Rey',
+  'Boxing',
+  'Frontend Developer',
+  'Career Progression',
+  'Object Oriented Programming',
+  'Film trailers',
+  'Best songs 2023',
+  'Makeup tutorial',
+]
